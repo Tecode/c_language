7bf796da60c553016599c9539830be8867d4f83f
Tecode <283731869@qq.com>
2021-12-18 20:38:04 +0800

    arm64和X86_64的汇编区别

---
+++ C语言编译成汇编/helloWorld.X86_64.s
@@ -0,0 +1,59 @@
+	.file	"helloWorld.c"
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "--->>\0"
+	.text
+	.globl	hello
+	.def	hello;	.scl	2;	.type	32;	.endef
+	.seh_proc	hello
+hello:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	leaq	.LC0(%rip), %rcx
+	call	printf
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC1:
+	.ascii "%d\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$4, -4(%rbp)
+	movl	$6, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -12(%rbp)
+	movl	-12(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC1(%rip), %rcx
+	call	printf
+	call	hello
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
