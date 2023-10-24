3b01ad7757a1dda854c53183ed3dc984cdbcb69b
Tecode <283731869@qq.com>
2023-10-24 15:46:14 +0800

    PlatformIO embedded

---
+++ Embedded/helloEmbedded/src/main.c
@@ -0,0 +1,11 @@
+#include "../include/mcs51/8051.h"
+#include "../include/caculater.h"
+
+int main() {
+// write your code here
+    P0 = 0xFE;
+    while (1) {
+        break;
+    }
+    return 0;
+}
