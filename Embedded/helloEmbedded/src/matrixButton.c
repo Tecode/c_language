fb5ac5f67341e1f1c1d6b21440c31629be756a06
haoxuan <haoxuan1009@foxmail.com>
2023-11-02 22:04:31 +0800

    feat:液晶显示屏实现输入密码功能

--- Embedded/helloEmbedded/src/matrixButton.c
+++ Embedded/helloEmbedded/src/matrixButton.c
@@ -7,6 +7,8 @@
 #include "common.h"
 #include "LCD1602.h"
 
+#define Password 7758
+
 unsigned char matrixKeyValue() {
     unsigned char keyValue = 0;
     // 按列扫描P13 P12 P11 P10
@@ -124,13 +126,33 @@ unsigned char matrixKeyValue() {
 
 
 unsigned char keyNumber;
-// 按下按钮显示数字
+unsigned char count = 0;
+unsigned int password = 0;
+
+// 按下按钮显示数字,实现4位数密码 S1-S9
+// S10 确定 S11取消
 void matrixKeyDown() {
     LCD_Init();
-    LCD_ShowString(1, 1, "MatrixNumber:");
+    LCD_ShowString(1, 1, "Password:");
     while (1) {
-        if (keyNumber) {
-            LCD_ShowNum(1, 2, keyNumber, 2);
+        keyNumber = matrixKeyValue();
+        if (keyNumber && count < 4 && keyNumber < 10) {
+            count++;
+            password = password * 10 + keyNumber;
+            LCD_ShowNum(2, 1, password, 4);
+        }
+        if (keyNumber == 10) {
+            if (Password == password) {
+                LCD_ShowString(1, 14, "OK");
+            } else {
+                LCD_ShowString(1, 14, "ERR");
+            }
+        }
+        if (keyNumber == 11) {
+            password = 0;
+            count = 0;
+            LCD_ShowNum(2, 1, password, 4);
+            break;
         }
     }
 }
