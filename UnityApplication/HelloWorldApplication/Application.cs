c381c2b7bc132371f42c066fc20ed642ad2635a3
Tecode <283731869@qq.com>
2024-10-18 11:12:19 +0800

    feat:C# hello world

---
+++ UnityApplication/HelloWorldApplication/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldApplication
+{
+    internal class Application
+    {
+        public static void Main(string[] args)
+        {
+            for (int index = 0; index < 10; index++)
+            {
+                Console.WriteLine("Hello World haoxuan {0}", index);
+            }
+            Console.ReadKey();
+        }
+    }
+}
