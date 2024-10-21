db6d4e8f3c1f8a0accaee39b2d1b49c5cbd6f023
Tecode <283731869@qq.com>
2024-10-21 23:40:57 +0800

    feat:增加windows运行的配置文件

---
+++ UnityApplication/FunctionApplication/Application.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace FunctionApplication // Note: actual namespace depends on the project name.
+{
+    internal abstract class Application
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Application!");
+        }
+    }
+}
